20fec7a91a1fd551b0fbcacc816f7dc4fc450d79
Thomas Mickley-Doyle <tmickleydoyle@gmail.com>
2023-04-17 17:31:45 -0500

    Add first two jobs out of HS

--- components/Resume.jsx
+++ components/Resume.jsx
@@ -17,16 +17,16 @@ const Resume = () => {
             ))}
             <br />
             <h1>Current</h1>
-            <h2>Title: {data.current_role}</h2>
-            <h2>Company: {data.current_company}</h2>
+            <h2>{data.current_role}</h2>
+            <h2>{data.current_company}</h2>
             <br />
             {data.previous_roles && data.previous_roles.length > 0 && (
             <h1>Previous</h1>
             )}
             {data.previous_roles && data.previous_roles.length > 0 && data.previous_roles.map((job, index) => (
             <div key={index}>
-                <h2>Title: {job.role}</h2>
-                <h2>Company: {job.company}</h2>
+                <h2>{job.role}</h2>
+                <h2>{job.company}</h2>
                 <br />
             </div>
             ))}
