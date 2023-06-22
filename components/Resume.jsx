32f3537119feb6a2b72c87521b6fc9dd621a68ac
Thomas Mickley-Doyle <tmickleydoyle@gmail.com>
2023-06-22 14:17:59 -0500

    update layout

--- components/Resume.jsx
+++ components/Resume.jsx
@@ -7,18 +7,14 @@ const Resume = () => {
             <h1>Location</h1>
             <h2>{data.location}</h2>
             <br />
-            {data.education && data.education.length > 0 && (
-            <h1>Education</h1>
-            )}
-            {data.education && data.education.length > 0 && data.education.map((edu, index) => (
+            <h1>Current</h1>
+           {data.current_roles && data.current_roles.length > 0 && data.current_roles.map((job, index) => (
             <div key={index}>
-                <h2>{edu.degree}, {edu.school}</h2>
+                <h2>{job.role}</h2>
+                <h3>{job.company}</h3>
+                <br />
             </div>
             ))}
-            <br />
-            <h1>Current</h1>
-            <h2>{data.current_role}</h2>
-            <h2>{data.current_company}</h2>
             <br />
             {data.previous_roles && data.previous_roles.length > 0 && (
             <h1>Previous</h1>
@@ -26,8 +22,17 @@ const Resume = () => {
             {data.previous_roles && data.previous_roles.length > 0 && data.previous_roles.map((job, index) => (
             <div key={index}>
                 <h2>{job.role}</h2>
-                <h2>{job.company}</h2>
+                <h3>{job.company}</h3>
                 <br />
+            </div>
+            ))}
+            <br />
+            {data.education && data.education.length > 0 && (
+            <h1>Education</h1>
+            )}
+            {data.education && data.education.length > 0 && data.education.map((edu, index) => (
+            <div key={index}>
+                <h2>{edu.degree}, {edu.school}</h2>
             </div>
             ))}
         </div>
