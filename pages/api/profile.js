b55ca852fda569d6c91a8474a0fce611d853805d
Thomas Mickley-Doyle <tmickleydoyle@gmail.com>
2023-10-30 12:40:34 -0500

    Add edge function to api

--- pages/api/profile.js
+++ pages/api/profile.js
@@ -1,6 +1,11 @@
 import path from 'path';
 import { promises as fs } from 'fs';
 
+export const config = {
+  runtime: 'edge',
+  regions: ['iad1'],
+};
+
 export default async function handler(req, res) {
   const jsonDirectory = path.join(process.cwd());
   const profileData = await fs.readFile(jsonDirectory + '/data/config.json', 'utf8');
