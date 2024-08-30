c68783997c60c358d3b03bc1349e3dec44d8a890
Thomas Mickley-Doyle <tmickleydoyle@gmail.com>
2024-08-29 21:20:01 -0500

    Update Resume.jsx

--- components/Resume.jsx
+++ components/Resume.jsx
@@ -1,44 +1,49 @@
 import data from "../data/config.json";
 
 const Resume = () => {
-  return(
+  return (
     <>
-        <div className="greytext">
-            <h1>Location</h1>
-            <h2>{data.location}</h2>
+      <div className="greytext">
+        <h1>Location</h1>
+        <h2>{data.location}</h2>
+        <br />
+
+        <h1>Current</h1>
+        {data.current_roles && data.current_roles.length > 0 && data.current_roles.map((job, index) => (
+          <div key={index}>
+            <h2>{job.role}</h2>
+            <h3>{job.company}</h3>
+            {job.summary && <p>{job.summary}</p>}
             <br />
-            <h1>Current</h1>
-           {data.current_roles && data.current_roles.length > 0 && data.current_roles.map((job, index) => (
-            <div key={index}>
-                <h2>{job.role}</h2>
-                <h3>{job.company}</h3>
-                <br />
-            </div>
-            ))}
+          </div>
+        ))}
+        <br />
+
+        {data.previous_roles && data.previous_roles.length > 0 && (
+          <h1>Previous</h1>
+        )}
+        {data.previous_roles && data.previous_roles.length > 0 && data.previous_roles.map((job, index) => (
+          <div key={index}>
+            <h2>{job.role}</h2>
+            <h3>{job.company}</h3>
+            {job.summary && <p>{job.summary}</p>}
             <br />
-            {data.previous_roles && data.previous_roles.length > 0 && (
-            <h1>Previous</h1>
-            )}
-            {data.previous_roles && data.previous_roles.length > 0 && data.previous_roles.map((job, index) => (
-            <div key={index}>
-                <h2>{job.role}</h2>
-                <h3>{job.company}</h3>
-                <br />
-            </div>
-            ))}
-            <br />
-            {data.education && data.education.length > 0 && (
-            <h1>Education</h1>
-            )}
-            {data.education && data.education.length > 0 && data.education.map((edu, index) => (
-            <div key={index}>
-                <h2>{edu.degree}</h2>
-                <h3>{edu.school}</h3>
-            </div>
-            ))}
-        </div>
+          </div>
+        ))}
+        <br />
+
+        {data.education && data.education.length > 0 && (
+          <h1>Education</h1>
+        )}
+        {data.education && data.education.length > 0 && data.education.map((edu, index) => (
+          <div key={index}>
+            <h2>{edu.degree}</h2>
+            <h3>{edu.school}</h3>
+          </div>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Resume;
