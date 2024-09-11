29162c9e045e6210e616aeee38f66c72225b5a81
Thomas Mickley-Doyle <tmickleydoyle@gmail.com>
2024-09-11 16:23:08 -0500

    u

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,7 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 import data from "../data/config.json";
 import Contributions from "./Contributions";
@@ -13,124 +15,106 @@ import StartProjectDoc from "./StartProjectDoc";
 import MetricsMethodologyDoc from "./MetricsMethodologyDoc";
 import PythonCodeBox from "./PythonBuildingTrees";
 
-const Profile = () => {
+export default function Profile() {
+  const router = useRouter();
   const [tab, setTab] = useState("profile");
+
+  useEffect(() => {
+    const { tab: urlTab } = router.query;
+    if (urlTab) {
+      setTab(urlTab);
+    }
+  }, [router.query]);
 
   const handleClick = (param) => {
     setTab(param);
+    router.push(`?tab=${param}`, undefined, { shallow: true });
   };
 
   return (
-    <>
-      <div className="maincontainer">
-        <div className="photoborder">
-          <Image
-            src={data.profile_photo}
-            width={250}
-            height={250}
-            alt="Profile Image"
-          />
-        </div>
-        <br />
-        <h1>{data.name}</h1>
-        <div className="greytext">
-          <a>&nbsp;</a>
-          {data.custom_contact_links &&
-            data.custom_contact_links.length > 0 &&
-            data.custom_contact_links.map((link, index) => (
-              <a key={link.name}>
-                <Link href={link.url}>{link.name}</Link>{" "}
-                {index !== data.custom_contact_links.length - 1 && " | "}{" "}
-              </a>
-            ))}
-        </div>
-        <br />
-        <hr width="40%" color="grey" size="2px" align="left" />
-        <br />
+    <div className="maincontainer">
+      <div className="photoborder">
+        <Image
+          src={data.profile_photo}
+          width={250}
+          height={250}
+          alt="Profile Image"
+        />
+      </div>
+      <br />
+      <h1>{data.name}</h1>
+      <div className="greytext">
+        <span>&nbsp;</span>
+        {data.custom_contact_links &&
+          data.custom_contact_links.length > 0 &&
+          data.custom_contact_links.map((link, index) => (
+            <span key={link.name}>
+              <Link href={link.url}>{link.name}</Link>{" "}
+              {index !== data.custom_contact_links.length - 1 && " | "}{" "}
+            </span>
+          ))}
+      </div>
+      <br />
+      <hr className="w-2/5 border-gray-300 border-t-2 my-4" />
+      <br />
+      <div className="space-x-2">
         <button className="customButton" onClick={() => handleClick("profile")}>
           Profile
         </button>
-        <a>|</a>
+        <span>|</span>
         <button
           className="customButton"
           onClick={() => handleClick("projects")}
         >
           Project Prioritization Planning
         </button>
-        <a>|</a>
+        <span>|</span>
         <button className="customButton" onClick={() => handleClick("start")}>
           Scoping New Data Science Projects
         </button>
-        <a>|</a>
+        <span>|</span>
         <button className="customButton" onClick={() => handleClick("metrics")}>
           Metrics Methodology
         </button>
-        <br />
-        <br />
+      </div>
+      <br />
+      <div className="space-x-2">
         <button className="customButton" onClick={() => handleClick("commits")}>
           GitHub Commits
         </button>
-        <a>|</a>
+        <span>|</span>
         <button className="customButton" onClick={() => handleClick("stars")}>
           GitHub Stars
         </button>
-        <a>|</a>
+        <span>|</span>
         <button
           className="customButton"
           onClick={() => handleClick("forecast")}
         >
           Linear Regression Tool
         </button>
-        <a>|</a>
+        <span>|</span>
         <button
           className="customButton"
           onClick={() => handleClick("treeBuilding")}
         >
           Python: Tree Builder Class
         </button>
-        <br />
-        {tab === "profile" && <Resume />}
-        {tab === "projects" && (
-          <div>
-            <PrioritizationDoc />
-          </div>
-        )}
-        {tab === "start" && (
-          <div>
-            <StartProjectDoc />
-          </div>
-        )}
-        {tab === "metrics" && (
-          <div>
-            <MetricsMethodologyDoc />
-          </div>
-        )}
-        {tab === "commits" && (
-          <div>
-            <Contributions />
-          </div>
-        )}
-        {tab === "stars" && (
-          <div>
-            <Stars />
-          </div>
-        )}
-        {tab === "forecast" && (
-          <div>
-            <Forecast />
-          </div>
-        )}
-        {tab === "treeBuilding" && (
-          <div>
-            <PythonCodeBox
-              code={data.tree_building_code}
-              title="Python Code: Tree Builder Class"
-            />
-          </div>
-        )}
       </div>
-    </>
+      <br />
+      {tab === "profile" && <Resume />}
+      {tab === "projects" && <PrioritizationDoc />}
+      {tab === "start" && <StartProjectDoc />}
+      {tab === "metrics" && <MetricsMethodologyDoc />}
+      {tab === "commits" && <Contributions />}
+      {tab === "stars" && <Stars />}
+      {tab === "forecast" && <Forecast />}
+      {tab === "treeBuilding" && (
+        <PythonCodeBox
+          code={data.tree_building_code}
+          title="Python Code: Tree Builder Class"
+        />
+      )}
+    </div>
   );
-};
-
-export default Profile;
+}
